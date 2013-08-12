be30606be48740b41614ab22e96895cb98442df0
Marshall <mad.mod.max@gmail.com>
2013-08-12 18:23:38 -0400

    Toggling start and stop buttons working.

--- pomodoro.js
+++ pomodoro.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+	//$('#controls').html('<button type="button" class="btn btn-lg btn-primary" id="start"><span class="button-text" id="start-text">开始</span></button>');
+	
 	$('#start').click(function() {
 		console.log('start');
+		//$('#controls').html('<button type="button" class="btn btn-lg btn-danger" id="stop"><span class="button-text" id="stop-text">结束</span></button>');
+		$('#start').css('display', 'none');
+		$('#stop').css('display', 'inline');
+	});
+	
+	$('#stop').click(function() {
+		console.log('stopped');
+		//$('#controls').html('<button type="button" class="btn btn-lg btn-primary" id="start"><span class="button-text" id="start-text">开始</span></button>');
+		$('#stop').css('display', 'none');
+		$('#start').css('display', 'inline');
 	});
 });
